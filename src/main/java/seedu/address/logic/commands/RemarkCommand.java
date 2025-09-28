97556009c9981d020c1a87a0132cb880fd8444a7
Jerrytys <jerrytys03@gmail.com>
2025-09-29 00:38:07 +0800

    Enhance RemarkCommand

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -1,5 +1,8 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
@@ -18,9 +21,41 @@ public class RemarkCommand extends Command {
             + "Likes hiking and outdoor activities";
     public static final String MESSAGE_NOT_IMPLEMENTED_YET = "This feature is not implemented yet.";
 
+    public static final String MESSAGE_ARGUMENTS = "Index: %1$d, Remark: %2$s";
+
+    private final Index index;
+    private final String remark;
+
+    /**
+     * Creates a RemarkCommand to add a remark to the specified {@code Person}
+     * @param index of the person in the filtered person list to edit
+     * @param remark of the person
+     */
+    public RemarkCommand(Index index, String remark) {
+        requireAllNonNull(index, remark);
+        this.index = index;
+        this.remark = remark;
+    }
+
     @Override
     public CommandResult execute(Model model) throws CommandException {
         // Implementation for adding a remark to a person
-        throw new CommandException(MESSAGE_NOT_IMPLEMENTED_YET);
+        throw new CommandException(String.format(MESSAGE_ARGUMENTS, index.getOneBased(), remark));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof RemarkCommand)) {
+            return false;
+        }
+
+        RemarkCommand e = (RemarkCommand) other;
+        return index.equals(e.index)
+                && remark.equals(e.remark);
     }
 }
